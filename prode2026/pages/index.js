7d08a4d24105d4bc3feb1bd44ec0140f6de0cf7e
jona6491 <jona6491@gmail.com>
2026-05-08 21:49:53 -0300

    Update index.js

--- prode2026/pages/index.js
+++ prode2026/pages/index.js
@@ -158,7 +158,15 @@ export default function Home() {
               </div>
               <div className="field">
                 <label>Tu nombre</label>
-                <input type="text" value={nombre} onChange={e=>setNombre(e.target.value)} autoComplete="off" />
+                <input
+  type="password"
+  maxLength={8}
+  value={clave}
+  placeholder=""
+  autoComplete="new-password"
+  onChange={e=>setClave(e.target.value)}
+  onKeyDown={e=>e.key==='Enter'&&handleLogin()}
+/>
               </div>
               <div className="field">
                 <label>Nombre de tu equipo</label>
